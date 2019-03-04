9aff155c9b766694d6d81a941c23788f21fea6d0
Donald P. Polansky <zizuar@comcast.net>
2019-03-04 16:45:38 -0600

    Show List

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,19 @@ var connection = mysql.createConnection({
         database: "bamazon_db"
     });
 
+    connection.connect(function(err){
+        if(err) throw err;
+        itemList();
+    });
+        
+function itemList(){
+    connection.query("SELECT * FROM product", function(err, results){
+        if(err) throw err;
+        console.log("id:\t\tProduct\t\tDepartment\t\tPrice\t\tQuantity");
+        console.log("=/\=========================================Bamazon=/\=Marketplace==========================/\=");
+        for(let i = 0; i<results.length; i++){
+        console.log(results[i].item_id +"\t\t"+ results[i].product_name +"\t\t"+ results[i].department_name +"\t\t"+results[i].price +"\t\t"+results[i].stock_quantity);
+            }
+        buyItem();
+    });
+}
